3611ec4424e7fac837197417a57faa0c319047c7
christian-calabrese <christian.calabrese@outlook.com>
2022-06-24 22:47:45 +0200

    fix(environment): enhanced environment file to clean app.py file up

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import boto3
-import json
 import os
 
 from aws_cdk import core
@@ -10,20 +8,8 @@ from infrastructure.utils.environment import Environment
 
 app = core.App()
 env_name = os.environ.get("ENVIRONMENT", "dev")
-with open(f"infrastructure/parameters/{env_name}.json", "r") as f:
-    params = json.loads(f.read(), object_hook=lambda d: Environment(**d))
 
-if os.path.isfile("infrastructure/parameters/uncommitted/.env.json"):
-    with open("infrastructure/parameters/uncommitted/.env.json", "r") as f:
-        uncommitted_env = json.loads(f.read())
-else:
-    secrets_manager = boto3.client("secretsmanager")
-    secret = secrets_manager.get_secret_value(
-            SecretId=params.github_token_secret_name
-        )
-    uncommitted_env = json.loads(secret['SecretString'])
-
-params.__dict__.update(uncommitted_env)
+params = Environment.from_file(env_path=f"infrastructure/parameters/{env_name}.json", uncommitted_env_path="infrastructure/parameters/uncommitted/.env.json")
 
 main_stack = InfrastructureStack(app, "CC-MainStack",
                                  env=core.Environment(region="eu-west-1"), params=params)
--- infrastructure/utils/environment.py
+++ infrastructure/utils/environment.py
@@ -1,6 +1,31 @@
+from __future__ import annotations
+
+import boto3
+import json
+import os
+
 from types import SimpleNamespace
+from typing import Optional
 
 
 class Environment(SimpleNamespace):
     def get(self, key, default):
         return self.__dict__.get(key, default)
+
+    @classmethod
+    def from_file(cls, env_path: str, uncommitted_env_path: Optional[str]) -> Environment:
+        instance = cls()
+        with open(env_path, "r") as f:
+            instance = json.loads(f.read(), object_hook=lambda d: cls(**d))
+
+        if uncommitted_env_path:
+                with open(uncommitted_env_path, "r") as f:
+                    uncommitted_env = json.loads(f.read())
+        else:
+            secrets_manager = boto3.client("secretsmanager")
+            secret = secrets_manager.get_secret_value(
+                    SecretId=instance.github_token_secret_name
+                )
+            uncommitted_env = json.loads(secret['SecretString'])
+        instance.__dict__.update(uncommitted_env)
+        return instance
